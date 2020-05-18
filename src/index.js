940f3e103b4ec654cd62862dc77abd7dda559a58
tomoyaueno29 <u.t01280729@gmail.com>
2020-05-18 16:34:28 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+<<<<<<< Updated upstream
 import './index.css';
 import App from './App';
+=======
+import App from './components/App';
+>>>>>>> Stashed changes
 import registerServiceWorker from './registerServiceWorker';
+import * as firebase from "firebase";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
+import 'semantic-ui-css/semantic.min.css'
+
+const Root = () => (
+  <Router>
+    <Switch>
+      <Route path="" component={App} />
+      <Route path="/login" component={Login} />
+      <Route path="/register" component={Register} />
+    </Switch>
+  </Router>
+)
+
+
+ReactDOM.render(<Root />, document.getElementById('root'));
 registerServiceWorker();
+
+
